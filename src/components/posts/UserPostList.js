63654822c1037da80773a63bbcef323acfa707e0
Jacob Eckert <jweckert17@gmail.com>
2020-10-22 14:34:23 -0500

    implement UserPostList and some basic styling for it

--- src/components/posts/UserPostList.js
+++ src/components/posts/UserPostList.js
@@ -1,7 +1,25 @@
-import React from "react"
+import React, { useEffect, useContext } from "react"
+import { Link } from "react-router-dom"
+import ListGroup from "react-bootstrap/ListGroup"
+import { PostContext } from "./PostProvider"
+import { UserPostListItem } from "./UserPostListItem"
 
 export const UserPostList = props => {
+  const { posts, getPostsByUserId } = useContext(PostContext)
+
+  useEffect(() => {
+    getPostsByUserId(localStorage.getItem('rare_user_id'))
+  }, [])
+
   return (
-    <div>User posts</div>
+    <div className="userPostList">
+      <ListGroup>
+        { posts.map(post => (
+          <ListGroup.Item as={Link} to={`/posts/${post.id}`}>
+            <UserPostListItem post={post} />
+          </ListGroup.Item>
+        ))}
+      </ListGroup>
+    </div>
   )
 }
