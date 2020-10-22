7d765efe29161ec15236e456d5985c0e65a78441
Jacob Eckert <jweckert17@gmail.com>
2020-10-22 09:21:50 -0500

    added /logout route handler, implementing a way for the user to log out

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,12 +1,11 @@
 import React from "react"
-import { Route } from "react-router-dom"
+import { Route, Redirect } from "react-router-dom"
 
 export const ApplicationViews = () => {
     return <>
-        <main style={{
-            margin: "5rem 2rem",
-            lineHeight: "1.75rem"
-        }}>
-        </main>
+      <Route path="/logout" render={() => {
+        localStorage.removeItem("rare_user_id")
+        return <Redirect to="/login" />
+      }} />
     </>
 }
