8fe1ce2060e75b8ddb0f4fd3c0b85c725a791109
mgjeffries <gib.jeffries@gmail.com>
2020-10-23 13:07:41 -0500

    rough draft of post tag form

---
+++ src/components/postTags/PostTagForm.js
@@ -0,0 +1,50 @@
+import React, { useContext, useEffect, useState } from "react"
+import { TagContext } from "../tags/TagProvider"
+import Button from "react-bootstrap/Button"
+
+
+export const PostTagForm = ({post}) => {
+	const { tags, getTags } = useContext(TagContext)
+	const [ thisPostTags, setThisPostTags ] = useState([]) //Update to use context when the provider is available
+
+
+	useEffect( () => {
+		getTags()
+	}, [] )
+
+	ToggleButtonGroupControlled = () => {
+		const [value, setValue] = useState([1, 3]);
+	
+		/*
+		 * The second argument that will be passed to
+		 * `handleChange` from `ToggleButtonGroup`
+		 * is the SyntheticEvent object, but we are
+		 * not using it in this example so we will omit it.
+		 */
+		const handleChange = (val) => setValue(val);
+	
+		return (
+			<ToggleButtonGroup type="checkbox" value={value} onChange={handleChange}>
+				<ToggleButton value={1}>Option 1</ToggleButton>
+				<ToggleButton value={2}>Option 2</ToggleButton>
+				<ToggleButton value={3}>Option 3</ToggleButton>
+			</ToggleButtonGroup>
+		);
+	}
+	
+
+	return (
+		<>
+			<div>
+				{
+					tags.map( tag => {
+						<Button variant="primary" size="sm">
+    				  {tag.name}
+    				</Button>
+					})					
+				}
+			</div>
+		</>
+	)
+
+}
