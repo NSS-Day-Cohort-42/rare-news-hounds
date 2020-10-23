d19c6191cb85ba455549159a37eba8e8526df66b
FORDBA <bfordcpa@gmail.com>
2020-10-23 13:32:00 -0500

    added postTag provider

---
+++ src/components/posttags/PostTagProvider.js
@@ -0,0 +1,43 @@
+import React, { useState, createContext } from "react";
+
+export const PostContext = createContext();
+
+export const PostProvider = props => {
+    const [thisPostTags, setPostTags] = useState([]);
+
+
+    const getPostTagsByPostId = postId => {
+        return fetch(`http://localhost:8088/post_tags?post_id=${postId}`)
+            .then(res => res.json())
+            .then(setPostTags);
+    };
+
+    const addPostTag = postTag => {
+        return fetch("http://localhost:8088/post_tags", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(postTag)
+        })
+            .then(getPostTagsByPostId)
+    }
+
+    const deletePostTag = (postTagId) => {
+        return fetch(`http://localhost:8088/post_tags/${postTagId}`, {
+            method: "DELETE"
+        })
+            .then(getPostTagsByPostId)
+    }
+
+    return (
+        <PostTagContext.Provider
+            value={{
+                thisPostTags,
+                getPostTagsByPostId, addPostTag, deletePostTag
+            }}
+        >
+            {props.children}
+        </PostTagContext.Provider>
+    )
+};
