4a6455df642e70dbdfc2941352a417139b2641b6
Gavin Schriver <alexanderschriver@gmail.com>
2020-10-23 15:52:32 -0500

    mt pr checkout

--- src/components/posts/PostDetail.js
+++ src/components/posts/PostDetail.js
@@ -1,10 +1,11 @@
 import moment from "moment";
 import React, { useContext, useState, useEffect } from "react";
 import { Image } from "react-bootstrap";
+import { ConfirmableDeleteButton } from "./ConfirmableDeleteButton";
 import { PostContext } from "./PostProvider";
 
 export default (props) => {
-  const { getPostById } = useContext(PostContext);
+  const { getPostById, deletePost } = useContext(PostContext);
   const [post, setPost] = useState({ category: {}, user: {} });
 
   useEffect(() => {
@@ -12,20 +13,22 @@ export default (props) => {
     getPostById(postId).then(setPost);
   }, []);
 
+  const currentUser = localStorage.getItem("rare_user_id");
   const date = moment(post.publication_time).format(
     "dddd, MMMM Do YYYY, h:mm:ss a"
   );
 
   return (
     <>
-      <section>
-        {date}
-      </section>
+      <section>{date}</section>
       <section>{post.title}</section>
       <section>{post.user.username}</section>
       <section>{post.category.name}</section>
       <Image src={post.image} />
       <section>{post.content}</section>
+          <ConfirmableDeleteButton onDelete={() => {
+              deletePost(post.id)
+          }}/>
     </>
   );
 };
