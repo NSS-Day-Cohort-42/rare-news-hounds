10e013219123828fa8cb41bc6c8befeffbacb169
Michael-Tyler <miketylerabc@gmail.com>
2020-10-23 09:15:49 -0500

    commit to merge main

--- src/components/posts/postForm.js
+++ src/components/posts/postForm.js
@@ -7,14 +7,20 @@ export const PostForm = (props) => {
 
     return (
         <Form>
-            <FormGroup controlId ="Email">
+            <FormGroup controlId ="email">
                 <Form.Label>Title</Form.Label>
                 <Form.Control as="text" placeholder="Enter Title" />
             </FormGroup>
-            <FormGroup controlId ="Email">
-                <Form.Label>Content</Form.Label>
-                <Form.Control as="Textarea" rows={3} placeholder="Enter post..." />
-            </FormGroup>
+            <Form.Group controlId="categorySelect">
+            <Form.Label>Categories</Form.Label>
+                <Form.Control as="select">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+                </Form.Control>
+            </Form.Group>
             <FormGroup>
                 <Form.Label>Publish Date:</Form.Label>
                 <Form.Control type="date" /> 
@@ -22,6 +28,10 @@ export const PostForm = (props) => {
             <FormGroup controlId ="Email">
                 <Form.Label>Image</Form.Label>
                 <Form.Control as="Text" placeholder="Image Url" />
+            </FormGroup>
+            <FormGroup controlId ="Email">
+                <Form.Label>Content</Form.Label>
+                <Form.Control as="Textarea" rows={3} placeholder="Enter post..." />
             </FormGroup>
             <Button>Submit Post</Button>  
         </Form>    
