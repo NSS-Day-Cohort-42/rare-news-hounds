1f73d91c81e5d1c314a92de7ad15a1277d3b22c0
mgjeffries <gib.jeffries@gmail.com>
2020-10-26 10:51:46 -0500

    remove rough draft comment

--- src/components/postTags/PostTagForm.js
+++ src/components/postTags/PostTagForm.js
@@ -5,18 +5,6 @@ import ToggleButton from "react-bootstrap/ToggleButton"
 import { PostTagContext } from "./PostTagProvider"
 import Button from "react-bootstrap/esm/Button"
 
-/*
-The post tag form should take in a post as a prop, and let users select which of the existing tags they want to apply. This form should work for editing and creating. When the form is submitted, any postTags that have been un-selected should be deleted, and an postTags that have been selected should be added.
-
-    The post tag form should get the postTagsByPostId, and store them in an list of object called thisPostTags
-    Existing PostTags should be given a property changeState="existing"
-    When a user clicks on a tag that isn't in thisPostTags, it should be added, and given the changeState of "toCreate"
-    When a user clicks to de-activate a tag, it should be given the state of "toDelete" or removed from the PostTags list, if it hasn't been created.
-    When a user hits 'save' the "toCreate" postTags should be created, and the "toDelete" posts should be deleted.
-
-*/
-
-
 export const PostTagForm = ({postId}) => {
 	const { tags, getTags } = useContext(TagContext)
 	const { getPostTagsByPostId, addPostTag, deletePostTag } = useContext(PostTagContext)
