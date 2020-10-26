94a49748dba857e31129fa034709e8f28d20afbe
Michael-Tyler <miketylerabc@gmail.com>
2020-10-26 14:51:28 -0500

    removed some comments

--- src/components/posts/EditPostButton.js
+++ src/components/posts/EditPostButton.js
@@ -2,13 +2,6 @@ import React from "react"
 import { useHistory } from "react-router-dom"
 import { Button } from "react-bootstrap"
 
-// This component will hold the edit button that will send the user 
-// to a form to edit that selected post
-
-//Create a function that will be called when clicked.
-//it should pull in current post ID
-
-//Create an edit button
 export default (props) => {
     const history = useHistory()
    
