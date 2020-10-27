78bfaf2220a1c65743ab6d99cee562b1cf0373be
Jacob Eckert <jweckert17@gmail.com>
2020-10-27 11:30:42 -0500

    increase font size of navbar links

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -14,11 +14,11 @@ export const NavBar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link as={Link} to="/posts/create">New Post</Nav.Link>
-            <Nav.Link as={Link} to="/my-posts">My Posts</Nav.Link>
-            <Nav.Link as={Link} to="/tags">Tags</Nav.Link>
-            <Nav.Link as={Link} to="/categories">Categories</Nav.Link>
-            <Nav.Link as={Link} to="/logout">Logout</Nav.Link>
+            <Nav.Link className="navbar__item" as={Link} to="/posts/create">New Post</Nav.Link>
+            <Nav.Link className="navbar__item" as={Link} to="/my-posts">My Posts</Nav.Link>
+            <Nav.Link className="navbar__item" as={Link} to="/tags">Tags</Nav.Link>
+            <Nav.Link className="navbar__item" as={Link} to="/categories">Categories</Nav.Link>
+            <Nav.Link className="navbar__item" as={Link} to="/logout">Logout</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
