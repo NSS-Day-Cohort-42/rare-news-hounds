3c6eb42bc181f75b8a447da79a57cde7c3746b76
mgjeffries <gib.jeffries@gmail.com>
2020-10-27 09:37:28 -0500

    remove un-used state from postTagList

--- src/components/postTags/PostTagList.js
+++ src/components/postTags/PostTagList.js
@@ -5,17 +5,13 @@ import Badge from 'react-bootstrap/Badge'
 import "./PostTagForm.css"
 
 export const PostTagList = ({postId}) => {
-    const { tags, getTags } = useContext(TagContext)
     const { getPostTagsByPostId } = useContext(PostTagContext)
     const [ thisPostTags, setThisPostTags ] = useState([]) 
-		const [ selectedPostTags, setSelectedPostTags ] = useState([]) 
 		
     useEffect( () => {
-        getTags()
         getPostTagsByPostId(postId)
             .then(thisPostTagsInitial => {
                 setThisPostTags(thisPostTagsInitial)
-                setSelectedPostTags(thisPostTagsInitial.map(pt => pt.tag_id))
             })
     }, [postId] )
     
