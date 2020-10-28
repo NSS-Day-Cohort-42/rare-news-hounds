10a07f1a51a2652c2ce1d78bb29fa64a8c1d528c
Jacob Eckert <jweckert17@gmail.com>
2020-10-28 12:08:48 -0500

    swap out dummy comment data for real data from provider

--- src/components/comments/CommentList.js
+++ src/components/comments/CommentList.js
@@ -1,23 +1,15 @@
 import React, { useEffect, useContext } from "react"
 import ListGroup from "react-bootstrap/ListGroup"
-// import CommentContext from "./CommentProvider"
+import { CommentContext } from "./CommentProvider"
 import { Comment } from "./Comment"
 
 export const CommentList = props => {
   const { postId } = props
 
-  // const { comments, getCommentsByPostId } = useContext(CommentContext)
-  const comments = [
-    {id: 1, "content":"this is a greattt idea","timestamp": 3948699542 ,"post_id": 2, "user_id": 4,
-    user: {
-      first_name: 'Jacob',
-      last_name: 'Eckert',
-      username: 'jweckert17'
-    }}
-  ]
+  const { comments, getCommentsByPostId } = useContext(CommentContext)
 
   useEffect(() => {
-    // getCommentsByPostId(postId)
+    getCommentsByPostId(postId)
   }, [])
 
   return (
