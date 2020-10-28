35f26287d1878ba4dd2118e6963aa13586150c4e
Jacob Eckert <jweckert17@gmail.com>
2020-10-28 12:11:41 -0500

    implement delete comment

--- src/components/comments/Comment.js
+++ src/components/comments/Comment.js
@@ -1,11 +1,14 @@
-import React from "react"
+import React, { useContext } from "react"
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
-import { ConfirmableDeleteButton } from "../posts/ConfirmableDeleteButton"
+import { ConfirmableDeleteButton } from "../posts/ConfirmableDeleteButton" // confirmable delete button
+import { CommentContext } from "./CommentProvider"
 
 export const Comment = props => {
   const { comment } = props
   const { id, content, post_id, user_id, user } = comment
+
+  const { deleteComment } = useContext(CommentContext)
 
   return (
     <Row>
@@ -16,7 +19,7 @@ export const Comment = props => {
       {
         parseInt(localStorage.getItem("rare_user_id")) === user_id && (
           <Col sm="4">
-            <ConfirmableDeleteButton onDelete={() => alert(`delete ${id}, from post ${post_id}`)} />
+            <ConfirmableDeleteButton onDelete={() => deleteComment(id, post_id) } />
           </Col>
         )
       }
--- src/components/comments/CommentProvider.js
+++ src/components/comments/CommentProvider.js
@@ -11,8 +11,16 @@ export const CommentProvider = (props) => {
       .then(setComments);
   };
 
+  // delete the comment with the given id, then fetch comments for given post id and update state
+  const deleteComment = (comment_id, post_id) => {
+    return fetch(`http://localhost:8088/comments/${comment_id}`, {
+      method: "DELETE"
+    })
+      .then(() => getCommentsByPostId(post_id))
+  }
+
   return (
-    <CommentContext.Provider value={{ comments, getCommentsByPostId }}>
+    <CommentContext.Provider value={{ comments, getCommentsByPostId, deleteComment }}>
       {props.children}
     </CommentContext.Provider>
   );
