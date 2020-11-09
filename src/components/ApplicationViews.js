5fae585264b2ad64ed7bf0d928ee311e95950d42
Michael-Tyler <miketylerabc@gmail.com>
2020-11-09 15:58:25 -0600

    updated logout to remove token and user id

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -20,7 +20,8 @@ export const ApplicationViews = () => {
       <Route
         path="/logout"
         render={() => {
-          localStorage.removeItem("token");
+          localStorage.removeItem("rare_user_token");
+          localStorage.removeItem("rare_user_id");
           return <Redirect to="/login" />;
         }}
       />
