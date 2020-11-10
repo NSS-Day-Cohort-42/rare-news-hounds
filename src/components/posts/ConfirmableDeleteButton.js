dcec87316b4a23222a589d1f0e49f1aa45fef786
Jacob Eckert <jweckert17@gmail.com>
2020-11-10 13:13:24 -0600

    refactor ConfirmableDeleteButton to render a Modal instead of two buttons when clicked

--- src/components/posts/ConfirmableDeleteButton.js
+++ src/components/posts/ConfirmableDeleteButton.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react"
-import Button from "react-bootstrap/Button"
+import { Modal, Button, Row } from "react-bootstrap"
+
+import "./ConfirmableDeleteButton.css"
 
 /**
- * ConfirmableDeleteButton component - renders as a "Delete" button that, when clicked, requires the user to either confirm their deletion before actually deleting, or cancel their deletion.
+ * ConfirmableDeleteButton component - renders as a "Delete" button that, when clicked, renders a react-bootstrap Modal prompting the user to confirm if they truly want to delete the thing.
  * 
  * Pass a function implementing whatever you want to happen when the user confirms deletion as props.onDelete.
  */
@@ -14,31 +16,35 @@ export const ConfirmableDeleteButton = props => {
     setIsDeleting(false)
   }
 
-  const renderInitialDeleteButton = () => (
-    <Button variant="danger" onClick={() => setIsDeleting(true)}>
-      Delete
-    </Button>
-  )
-
-  const renderConfirmDeleteButtons = () => (
-    <>
-      <Button className="mr-1" variant="secondary" onClick={() => setIsDeleting(false)}>
-        Cancel
-      </Button>
-
-      <Button variant="danger" onClick={handleConfirmDelete}>
-        Confirm Delete
-      </Button>
-    </>
-  )
-
   // this div includes a click handler that calls preventDefault on the event when clicked
   // this is because one context in which the ConfirmableDeleteButton is used is in the UserPostListItem, which
   // is rendered within a Link, and if the default event behavior was allowed, clicking on the delete button
   // in that context would cause the user to navigate to that Link's "to" prop
-  return (
+  return <>
     <div className="confirmableDeleteButtonContainer d-flex justify-content-end mx-2" onClick={e => e.preventDefault()}>
-      { isDeleting ? renderConfirmDeleteButtons() : renderInitialDeleteButton() }
+        <Button variant="danger" onClick={() => setIsDeleting(true)}>
+          Delete
+        </Button>
     </div>
-  )
+
+    <Modal show={isDeleting} onHide={() => setIsDeleting(false)} className="confirmableDeleteModal">
+      <Modal.Dialog>
+        <Modal.Title>Confirm Delete</Modal.Title>
+        <Modal.Body>
+          <Row>
+            <p>{props.prompt || "Are you sure you want to delete this item?"}</p>
+          </Row>
+          <Row>
+            <Button className="mr-1" variant="secondary" onClick={() => setIsDeleting(false)}>
+              Cancel
+            </Button>
+
+            <Button variant="danger" onClick={handleConfirmDelete}>
+              Confirm Delete
+            </Button>
+          </Row>
+        </Modal.Body>
+      </Modal.Dialog>
+    </Modal>
+  </>
 }
