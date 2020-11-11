760b76f26d4d640f065d8c023e6d5f03d1a59319
Jacob Eckert <jweckert17@gmail.com>
2020-11-11 11:37:39 -0600

    create TagManager component to wrap the TagForm and TagList, route /tags to it in ApplicationViews

---
+++ src/components/tags/TagManager.js
@@ -0,0 +1,26 @@
+import React from "react"
+import { Row, Col } from "react-bootstrap"
+
+import TagForm from "./TagForm"
+import TagList from "./TagList"
+
+/**
+ * Component to wrap the TagForm and TagList, mostly for layout purposes
+ */
+export const TagManager = () => {
+
+  /* Row having flex-md-row-reverse className will cause the Form to appear on the right in medium-and-up
+  screen sizes, but to stack on top of the List in lower-than-medium screen sizes */
+  return (
+    <Row className="flex-md-row-reverse align-items-start justify-content-around">
+      <Col lg="5" sm="12" className="border border-dark rounded py-4">
+        <h2 className="text-center">Create a new tag</h2>
+        <TagForm />
+      </Col>
+      <Col lg="5" sm="12">
+        <h2 className="text-center my-4">Tags</h2>
+        <TagList />
+      </Col>
+    </Row>
+  )
+}
