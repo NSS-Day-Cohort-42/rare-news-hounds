315d81d6f8bd4d4892af7bb670f2152df24f848d
mgjeffries <gib.jeffries@gmail.com>
2020-11-11 15:39:20 -0600

    add explanatory comment to post tag form

--- src/components/postTags/PostTagForm.js
+++ src/components/postTags/PostTagForm.js
@@ -3,6 +3,9 @@ import { TagContext } from "../tags/TagProvider";
 import Button from "react-bootstrap/esm/Button";
 
 export const PostTagForm = ({selectedPostTagIds, onTogglePostTag}) => {
+  //This component renders each tag as button, styled as either primary or secondary,
+  //depending on whether the tag has been selected. The parent component gets to decide
+  //which tags have been selected, an what to do when the button is pressed.
   const { tags, getTags } = useContext(TagContext);
 
   useEffect(() => {
