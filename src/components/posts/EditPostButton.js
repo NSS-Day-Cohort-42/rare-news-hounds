6bb468ce3cd5a7168cdaf6a7ff52788b30829e2b
Jacob Eckert <jweckert17@gmail.com>
2020-11-12 10:45:15 -0600

    style UserPostListItem to resemble mockup

--- src/components/posts/EditPostButton.js
+++ src/components/posts/EditPostButton.js
@@ -1,16 +1,18 @@
 import React from "react"
 import { useHistory } from "react-router-dom"
 import { Button } from "react-bootstrap"
+import { BsGearFill } from "react-icons/bs"
 
 export default (props) => {
     const history = useHistory()
    
     return (
-        <Button onClick={evt => {
+        <Button className="border-0 bg-white text-dark"
+          onClick={evt => {
             evt.preventDefault()
             history.push(`/posts/edit/${props.postId}`)   
         }}>
-            Edit Post
+            <BsGearFill style={{ fontSize: '36px' }} />
         </Button>
     )
 }
--- src/components/posts/UserPostListItem.js
+++ src/components/posts/UserPostListItem.js
@@ -2,14 +2,13 @@ import React, { useContext } from "react"
 import { ConfirmableDeleteButton } from "./ConfirmableDeleteButton"
 import EditPostButton from "./EditPostButton"
 import { PostContext } from "./PostProvider"
-import Badge from "react-bootstrap/Badge"
-import Row from "react-bootstrap/Row"
-import Col from "react-bootstrap/Col"
+import { Row, Col, Image } from "react-bootstrap"
 import "./PostListItem.css"
 
 export const UserPostListItem = props => {
   const { post } = props
-  const { id, title, user, category, publication_date } = post
+  const { id, title, user, publication_date, image_url } = post
+  const readableDate = (new Date(publication_date + 'T00:00:00')).toLocaleDateString('en-Us')
 
   const { deletePost, getPostsByUserId } = useContext(PostContext)
 
@@ -19,22 +18,52 @@ export const UserPostListItem = props => {
   }
 
   return (
-    <Row className="align-items-center">
-      <Col sm="7">
-        <p className="postListItem__title text-primary my-1 mr-2 font-weight-bold">{title}</p>
-        <p className="postListItem__author text-uppercase my-1 mr-1">{user.fullname}</p>
-        <p className="postListItem__author text-uppercase my-1 mr-1">{publication_date}</p>
+    <div>
+      <Row className="align-items-center">
+        <Col sm="8">
+          <h2 className="font-weight-bold">{title}</h2>
         </Col>
-      <Col sm="5">
-        <Row className="justify-content-end align-items-center">
-          <Badge variant="info" className="postListItem__category">
-            <span className="postListItem__categoryName">{category.label}</span>
-          </Badge>
+        <Col sm="4">
+          <p className="text-right">Publication Date: {readableDate}</p>
+        </Col>
+      </Row>
 
-          <ConfirmableDeleteButton onDelete={() => handleDelete(id)} />
+      <Row className="justify-content-center">
+        <Image src={image_url} style={{ maxHeight: '300px' }} />
+      </Row>
+
+      <Row className="align-items-center">
+        <Col sm="3">
+          <p>Author: {user.fullname}</p>
+        </Col>
+        <Col sm="6" className="d-flex justify-content-end">
+          <div>Reactions Placeholder</div>
+        </Col>
+        <Col sm="3" className="d-flex justify-content-end">
           <EditPostButton postId={id} />
-        </Row>
-      </Col>
-    </Row>
+          <ConfirmableDeleteButton onDelete={() => handleDelete(id)} />
+        </Col>
+      </Row>
+    </div>
   )
+
+  // return (
+  //   <Row className="align-items-center">
+  //     <Col sm="7">
+  //       <p className="postListItem__title text-primary my-1 mr-2 font-weight-bold">{title}</p>
+  //       <p className="postListItem__author text-uppercase my-1 mr-1">{user.fullname}</p>
+  //       <p className="postListItem__author text-uppercase my-1 mr-1">{publication_date}</p>
+  //       </Col>
+  //     <Col sm="5">
+  //       <Row className="justify-content-end align-items-center">
+  //         <Badge variant="info" className="postListItem__category">
+  //           <span className="postListItem__categoryName">{category.label}</span>
+  //         </Badge>
+
+  //         <ConfirmableDeleteButton onDelete={() => handleDelete(id)} />
+  //         <EditPostButton postId={id} />
+  //       </Row>
+  //     </Col>
+  //   </Row>
+  // )
 }
