33b4c4330118a0732d3df94dd33758b97f414671
Gavin Schriver <alexanderschriver@gmail.com>
2020-11-12 16:02:38 -0600

    format tweak for comment buttons

--- src/components/comments/CommentList.js
+++ src/components/comments/CommentList.js
@@ -1,86 +1,77 @@
-import React, { useEffect, useContext, useState } from "react"
-import { ListGroup, Col, Row } from "react-bootstrap"
-import { CommentContext } from "./CommentProvider"
-import { ConfirmableDeleteButton } from "../posts/ConfirmableDeleteButton"
-import { ConfirmableEditCommentButton } from "./ConfirmableEditCommentButton"
+import React, { useEffect, useContext, useState } from "react";
+import { ListGroup, Col, Row } from "react-bootstrap";
+import { CommentContext } from "./CommentProvider";
+import { ConfirmableDeleteButton } from "../posts/ConfirmableDeleteButton";
+import { ConfirmableEditCommentButton } from "./ConfirmableEditCommentButton";
 
-
-export const CommentList = props => {
-  const { comments, getCommentsByPostId, deleteComment } = useContext(CommentContext)
+export const CommentList = (props) => {
+  const { comments, getCommentsByPostId, deleteComment } = useContext(
+    CommentContext
+  );
 
   useEffect(() => {
     const postId = parseInt(props.match.params.postId);
-    getCommentsByPostId(postId)
-    
-  }, [])
+    getCommentsByPostId(postId);
+  }, []);
 
   const confirmDelete = (commentId, props) => {
     const postId = parseInt(props.match.params.postId);
     deleteComment(commentId, postId);
   };
 
-  
   return (
     <div>
-      <div className="commentList" style={{ maxHeight: '250px', overflowY: 'scroll' }}>
+      <div
+        className="commentList"
+        style={{ maxHeight: "250px", overflowY: "scroll" }}
+      >
         <ListGroup>
-
-          {
-            comments.map(c => {
-              if (parseInt(localStorage.getItem("rare_user_id")) === c.author.id ) {
-                return (
-
-                  <div>
-                <ListGroup.Item key={c.id}>
-                 
-                 <Row className="justify-content-between">
-                  <h1>subject: {c.subject}</h1>
-                      
+          {comments.map((c) => {
+            if (
+              parseInt(localStorage.getItem("rare_user_id")) === c.author.id
+            ) {
+              return (
+                <div>
+                  <ListGroup.Item key={c.id}>
+                    <Row className="justify-content-between">
+                      <h1>subject: {c.subject}</h1>
+                      <div className="d-flex">
+                        <ConfirmableEditCommentButton
+                          comment={c}
+                          postId={parseInt(props.match.params.postId)}
+                        />
                         <ConfirmableDeleteButton
                           prompt="Are you sure you want to delete this comment?"
-                          onDelete={() => confirmDelete(c.id, props)} />
-                        <ConfirmableEditCommentButton comment={c} postId={parseInt(props.match.params.postId)}/>
-                      
-                   </Row>
-                  <h4>{c.content}</h4>
-                  <h4>{new Date(c.created_on).toLocaleDateString('en-Us')}</h4>
-                  <h4>{c.author && c.author.username}</h4>
-                 
-                  
-                  
-                  
-                  
-                  
-                </ListGroup.Item>
-
-              </div>
-                          )
-                        }
-              else {
-                return (
-
-                  <div>
-                <ListGroup.Item key={c.id}>
-                  <div comment={c.content} />
-                  <h1>subject: {c.subject}</h1>
-                  <h4>{c.content}</h4>
-                  <h4>{new Date(c.created_on).toLocaleDateString('en-Us')}</h4>
-                  <h4>{c.author && c.author.username}</h4>
-                  
-                  
-                  
-                </ListGroup.Item>
-
-              </div>
-                          )
-              }
-            })
-          }
+                          onDelete={() => confirmDelete(c.id, props)}
+                        />
+                      </div>
+                    </Row>
+                    <h4>{c.content}</h4>
+                    <h4>
+                      {new Date(c.created_on).toLocaleDateString("en-Us")}
+                    </h4>
+                    <h4>{c.author && c.author.username}</h4>
+                  </ListGroup.Item>
+                </div>
+              );
+            } else {
+              return (
+                <div>
+                  <ListGroup.Item key={c.id}>
+                    <div comment={c.content} />
+                    <h1>subject: {c.subject}</h1>
+                    <h4>{c.content}</h4>
+                    <h4>
+                      {new Date(c.created_on).toLocaleDateString("en-Us")}
+                    </h4>
+                    <h4>{c.author && c.author.username}</h4>
+                  </ListGroup.Item>
+                </div>
+              );
+            }
+          })}
         </ListGroup>
-
       </div>
     </div>
-  )
-
-}
-
+  );
+};
