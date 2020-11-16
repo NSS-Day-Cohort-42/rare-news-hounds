a1427554efadaf2d26857784828e350b522fe00f
Jacob Eckert <jweckert17@gmail.com>
2020-11-16 13:31:49 -0600

    implement logic to PATCH for updating a post's approval state in provider, implement ApprovePostToggler component

--- src/components/posts/ApprovePostToggler.js
+++ src/components/posts/ApprovePostToggler.js
@@ -1,14 +1,24 @@
-import React from "react"
+import React, { useContext, useState } from "react"
+import { PostContext } from "./PostProvider"
 
 export const ApprovePostToggler = props => {
   const { postId, isApproved } = props;
 
+  const [ isSubmitting, setIsSubmitting ] = useState(false)
+
+  const { updatePostApproval } = useContext(PostContext)
+
   const handleToggleApproval = () => {
-    
+    setIsSubmitting(true)
+    const updatedIsApproved = !isApproved
+
+    updatePostApproval(postId, updatedIsApproved)
+      .then(() => setIsSubmitting(false))
   }
 
   return (
     <input type="checkbox" 
+      disabled={isSubmitting}
       name="approve_post" 
       checked={isApproved}
       onClick={handleToggleApproval} />
