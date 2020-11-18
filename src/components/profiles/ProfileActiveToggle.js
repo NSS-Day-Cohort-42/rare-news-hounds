b359ab935c86d584b08803c603fc77042dc03e66
mgjeffries <gib.jeffries@gmail.com>
2020-11-18 09:31:59 -0600

    add modal to confirm de-activation of profiles

--- src/components/profiles/ProfileActiveToggle.js
+++ src/components/profiles/ProfileActiveToggle.js
@@ -6,6 +6,24 @@ import { ProfileContext } from "./ProfileProvider";
 export default (props) => {
   const { userId, isActive } = props;
   const { updateProfile } = useContext(ProfileContext)
+  const [show, setShow] = useState(false);
+
+  const handleProfileToggle = () => {
+    if (isActive) {
+      // The user is selecting to de-activate a profile. 
+      // Show a modal to confirm de-activation
+      setShow(true)
+    }
+    else {
+      // The user is activating an inactive profile. 
+      updateProfile(userId, { active: true })
+    }
+  }
+
+  const deactivateAndClose = () => {
+    updateProfile(userId, { active: false })
+    setShow(false)
+  }
 
   return (
     <>
@@ -16,11 +34,35 @@ export default (props) => {
         name={`toggle_user_${userId}_role`}
         type="checkbox"
         checked={isActive}
-        onChange={() => {
-          updateProfile(userId, { active: !isActive })
-        }}
-        value={isActive}
+        onChange={handleProfileToggle}
       />
+      <Modal show={show} onHide={() => setShow(false)}>
+        <Modal.Dialog>
+          <Col>
+            <Modal.Title>Confirm Status Change</Modal.Title>
+            <Modal.Body>
+              <Row>
+                <p>
+                  Warning: This action will de-activate this user. Do you want to continue?
+                </p>
+              </Row>
+              <Row>
+                <Button
+                  className="mr-1"
+                  variant="secondary"
+                  onClick={() => setShow(false)}
+                >
+                  Cancel
+                </Button>
+
+                <Button variant="danger" onClick={deactivateAndClose}>
+                  De-activate User
+                </Button>
+              </Row>
+            </Modal.Body>
+          </Col>
+        </Modal.Dialog>
+      </Modal>
     </>
   );
 };
