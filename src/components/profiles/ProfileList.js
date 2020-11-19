80005c0bf3494e85f60cf4798ea79d841ce0be46
Dylan Rowe <dylanthk@gmail.com>
2020-11-19 09:55:53 -0600

    wip

--- src/components/profiles/ProfileList.js
+++ src/components/profiles/ProfileList.js
@@ -7,50 +7,57 @@ import ProfileActiveToggle from "./ProfileActiveToggle";
 import "./ProfileDetail.css"
 
 export const ProfileList = (props) => {
-  const { profiles, getProfiles } = useContext(ProfileContext);
+    const { profiles, getProfiles } = useContext(ProfileContext);
 
-  useEffect(() => {
-    getProfiles()
-  }, []);
+    useEffect(() => {
+        getProfiles()
+    }, []);
 
-  const alphabeticalUsers = profiles.sort((userId1, userId2) => {
-    return userId1.username.localeCompare(userId2.username);
-  })
+    const alphabeticalUsers = profiles.sort((userId1, userId2) => {
+        return userId1.username.localeCompare(userId2.username);
+    })
+ 
+    let counter = 0
+    profiles.forEach(user => {
+        if(user.is_staff) {
+            counter++
+        }
+    });
 
-  return (
-    <Table striped bordered hover size="sm" className="userProfileContainer">
-      <tbody>
-        {alphabeticalUsers.map((profile) => {
-          const isStaff = profile.is_staff;
-          return (
-            <tr>
-              <td><Link to={`/profiles/${profile.id}`}>{profile.username}</Link></td>
-                {
-                  localStorage.getItem("is_admin") &&
-                  <>
-                    <td>
-                      <ProfileActiveToggle
-                      isActive={profile.active}
-                      userId={profile.id}
-                      key={profile.id}
-                      />
-                    </td>
-                    <td>
-                    {localStorage.getItem("is_admin") &&
-                  <ProfileStatusToggle
+    return (
+        <Table striped bordered hover size="sm" className="userProfileContainer">
+            <tbody>
+                {alphabeticalUsers.map((profile) => {
+                    const isStaff = profile.is_staff;
+                    return (
+                        <tr>
+                            <td><Link to={`/profiles/${profile.id}`}>{profile.username}</Link></td>
+                            {
+                                localStorage.getItem("is_admin") &&
+                                <>
+                                    <td>
+                                        <ProfileActiveToggle
+                                            isActive={profile.active}
+                                            userId={profile.id}
+                                            key={profile.id}
+                                        />
+                                    </td>
+                                    <td>
+                                        {localStorage.getItem("is_admin") &&
+                                            <ProfileStatusToggle
 
-                    isStaff={profile.is_staff}
-                    userId={profile.id}
-                    canDeactivate={counter >=  2}
-                  />
-                }
-                    </td>
-                  </>
-                }
-            </tr>
-          );
-        })}
-      </tbody>
-    </Table>
-  );
+                                                isStaff={profile.is_staff}
+                                                userId={profile.id}
+                                                canDeactivate={counter >= 2}
+                                            />
+                                        }
+                                    </td>
+                                </>
+                            }
+                        </tr>
+                    );
+                })}
+            </tbody>
+        </Table>
+    );
 };
