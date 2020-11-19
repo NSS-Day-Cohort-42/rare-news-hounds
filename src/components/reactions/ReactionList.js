f371333ba6c1592e2204cfd37a99b41c55512306
Gavin Schriver <alexanderschriver@gmail.com>
2020-11-19 14:42:31 -0600

    should probably just stash lol

--- src/components/reactions/ReactionList.js
+++ src/components/reactions/ReactionList.js
@@ -8,7 +8,7 @@ import "./reactions.css";
 export default (props) => {
   const { postReactions, userReactions, postId, handleReaction } = props;
   const { reactions, getReactions } = useContext(ReactionContext);
-  const { addReaction, deleteReaction} = useContext(PostContext);
+  const { addReaction, deleteReaction } = useContext(PostContext);
   const [submitting, setSubmitting] = useState(false);
   const history = useHistory();
 
@@ -33,9 +33,9 @@ export default (props) => {
     getReactions();
   }, []);
 
-    // when a user clicks a reaction, if reactionValue is false, they're trying to delete,
-    // so do that; otherwise they're tyring to add, so do that. then we have to re-fetch our post
-    // and re-set it in PostDetail so it passes new accurate props to this component
+  // when a user clicks a reaction, if reactionValue is false, they're trying to delete,
+  // so do that; otherwise they're tyring to add, so do that. then we have to re-fetch our post
+  // and re-set it in PostDetail so it passes new accurate props to this component
   const handleReactionClick = (reactionValue, reactionId) => {
     if (reactionValue === false) {
       setSubmitting(true);
@@ -44,20 +44,22 @@ export default (props) => {
         handleReaction(postId);
       });
     } else {
-      setSubmitting(true)  
-        deleteReaction(reactionId, postId).then(() => {
-            setSubmitting(false)
-            handleReaction(postId)
-        })
+      setSubmitting(true);
+      deleteReaction(reactionId, postId).then(() => {
+        setSubmitting(false);
+        handleReaction(postId);
+      });
     }
   };
 
   return (
     <section className="reactions">
-          {reactionsWithCount.map((r) => {
-          // userReactions is an array of reactionIds for this post + user combo
-              const userHasReacted = userReactions.some((ur) => ur === r.id);
-              
+      {reactionsWithCount.map((r) => {
+        // userReactions is an array of reactionIds for this post + user combo,
+        //so the value of userHasReacted will tell the handler whether we
+        //need to created or delete a postreaction
+        const userHasReacted = userReactions.some((ur) => ur === r.id);
+
         return (
           <article className="reaction">
             <div className="reaction--count">{r.count}</div>
